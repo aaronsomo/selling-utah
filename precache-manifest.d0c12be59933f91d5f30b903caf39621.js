fe58a61b715d279a17de70f6954485f3a4f42070
Aaron Southammavong <aaroneight07@gmail.com>
2022-02-05 18:02:19 -0600

    Updates

---
+++ precache-manifest.d0c12be59933f91d5f30b903caf39621.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0116f80a9ec5dd0e785e23e50ae9d2fd",
+    "url": "/selling-utah/index.html"
+  },
+  {
+    "revision": "4caaab7f8aec2b1fd6cf",
+    "url": "/selling-utah/static/css/main.14e960f2.chunk.css"
+  },
+  {
+    "revision": "9354736a24108e12e999",
+    "url": "/selling-utah/static/js/2.68b4245f.chunk.js"
+  },
+  {
+    "revision": "4caaab7f8aec2b1fd6cf",
+    "url": "/selling-utah/static/js/main.6db5de8a.chunk.js"
+  },
+  {
+    "revision": "a517660085f1a7ef0b4b",
+    "url": "/selling-utah/static/js/runtime-main.b1f21374.js"
+  },
+  {
+    "revision": "f1f5b4f33b7056fdc84c17efa1c88be9",
+    "url": "/selling-utah/static/media/angelslanding.f1f5b4f3.jpeg"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/selling-utah/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/selling-utah/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/selling-utah/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/selling-utah/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/selling-utah/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "4c42c11f1fc0203483c21f2d6eeb8896",
+    "url": "/selling-utah/static/media/logo-green-1x.4c42c11f.png"
+  },
+  {
+    "revision": "3365abba6477c48e6be892cf6faa8f25",
+    "url": "/selling-utah/static/media/logo-green-2x.3365abba.png"
+  },
+  {
+    "revision": "888043d544c9ed77285afbc360ffd27e",
+    "url": "/selling-utah/static/media/logo-green-small-2x.888043d5.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/selling-utah/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "7fdf2a886516e020e300ae6bd544ab7d",
+    "url": "/selling-utah/static/media/nat-1-large.7fdf2a88.jpg"
+  },
+  {
+    "revision": "d6c15bf0efcaf4c03c5cb0c9078cdd17",
+    "url": "/selling-utah/static/media/nat-1.d6c15bf0.jpg"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/selling-utah/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "7a25f1999a15760508f5e6131245aeac",
+    "url": "/selling-utah/static/media/nat-2-large.7a25f199.jpg"
+  },
+  {
+    "revision": "4d3783688636b59662283e19991a2912",
+    "url": "/selling-utah/static/media/nat-2.4d378368.jpg"
+  },
+  {
+    "revision": "417d3d741a7a47b3d6ef0307228033a0",
+    "url": "/selling-utah/static/media/nat-3-large.417d3d74.jpg"
+  },
+  {
+    "revision": "cc2b05433cbad0b369d131d02c0cac6a",
+    "url": "/selling-utah/static/media/nat-3.cc2b0543.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/selling-utah/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/selling-utah/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/selling-utah/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/selling-utah/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "14efd82b81787a9764b2cb7eb7afd3e8",
+    "url": "/selling-utah/static/media/nat-8.14efd82b.jpg"
+  },
+  {
+    "revision": "b525a99bcf20d08b2aaf6dd83b7be623",
+    "url": "/selling-utah/static/media/nat-9.b525a99b.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/selling-utah/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/selling-utah/static/media/video.ffcd876c.mp4"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/selling-utah/precache-manifest.d0c12be59933f91d5f30b903caf39621.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/selling-utah/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
